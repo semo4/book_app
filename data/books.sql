f239a302b98c507f507ef06dee864756b1e1289f
semo4 <fotboy1788@hotmail.com>
2021-03-01 16:03:40 +0200

    add lab 12

---
+++ data/books.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS books ;
+
+CREATE TABLE IF NOT EXISTS
+books (
+  id SERIAL PRIMARY KEY NOT NULL,
+  author VARCHAR(256) NOT NULL UNIQUE,
+  title VARCHAR(256) NOT NULL UNIQUE,
+  isbn VARCHAR(256) NOT NULL,
+  image_url VARCHAR(500) NOT NULL,
+  description text NOT NULL
+);
+
+
+
+
--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.urlencoded({extended: true}));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 require('dotenv').config();
+let pg  = require('pg');
 const PORT = process.env.PORT;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
 
 app.get('/', hendleHome);
@@ -22,7 +26,7 @@ app.get('/search', hendleSearch);
 // app.get('/searches/new', handleSearchPage);
 app.post('/show', handleBookSearch);
 app.get('/error', handleError);
-
+app.post('/save', handlesave);
 app.get('*',handleerror);
 function handleerror(req, res){
     handleError(req,res);
@@ -39,17 +43,29 @@ function hendleSearch(req, res){
     res.render('pages/searches/new');
 }
 
-
+function handlesave(req, res){
+    let title = req.body.title;
+    let author =  req.body.authors;
+    let description =  req.body.description;
+    let image =  req.body.image;
+    let isbn =  req.body.isbn;
+    console.log('data returned back from db ',  [title,author, isbn, image, description]);
+    let sqlQuery = `insert into books(title,author, isbn, image_url, description) values ($1,$2,$3,$4,$5)returning *`;
+    let value = [title,author, isbn, image, description];
+    client.query(sqlQuery, value).then(data => {
+        console.log('data returned back from db ', data);
+        res.redirect('/');
+    });
+}
 
 function handleBookSearch(req, res){
     let searchquery = req.body.searchquery;
 
     let terms = req.body.select;
-    console.log(req);
-    console.log(terms);
+ 
     
     let concatSearch= "+in"+terms+':'+searchquery;
-    console.log(concatSearch);
+  
 
     let defaultInmge = "https://i.imgur.com/J5LVHEL.jpg";
 
@@ -68,25 +84,38 @@ function handleBookSearch(req, res){
         let author = obj.volumeInfo.authors;
         let description = obj.volumeInfo.description;
         let image = obj.volumeInfo.imageLinks.thumbnail;
+        let isbn = obj.volumeInfo.industryIdentifiers;
+        
         if(author === undefined){
             author = "Unkown Author";
         }
         if(description === undefined){
             description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellendus officia cupiditate id esse pariatur sint! Odio, veritatis accusamus. Fugit magni, provident quo unde nulla dicta voluptatum eum saepe rem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellendus officia cupiditate id esse pariatur sint! Odio, veritatis accusamus. Fugit magni, provident quo unde nulla dicta voluptatum eum saepe rem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellendus officia cupiditate id esse pariatur sint! Odio, veritatis accusamus. Fugit magni, provident quo unde nulla dicta voluptatum eum saepe rem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellendus officia cupiditate id esse pariatur sint! Odio, veritatis accusamus. Fugit magni, provident quo unde nulla dicta voluptatum eum saepe rem.";
         }
+        if(isbn===undefined){
+            isbn = "00000000";
+        }else{
+            isbn = isbn[0].identifier;
+        }
+      
         if(image === undefined){
             image = defaultInmge;
         }
         image = image.replace(/^http:\/\//i, 'https://');
 
-        
-        return new Book(title, author, description, image);
+        // let sqlQuery = `insert into citylocation(c_name,display_name, lat, lon) values ($1,$2,$3,$4)returning *`;
+        // let value = [searchQuery, displayName, latitude, longitude];
+        // client.query(sqlQuery, value).then(data => {
+        //     console.log('data returned back from db ', data);
+        // });
+        return new Book(title, author, description, image, isbn);
       });
  
         res.render('pages/searches/show', {books:arrayBooks });
 
     }).catch(error =>{
         handleError(req,res);
+        console.log(error);
 
     });
 
@@ -95,12 +124,21 @@ function handleBookSearch(req, res){
 
 }
 
-function Book(title, author, description, image){
+function Book(title, author, description, image,isbn){
     this.title= title;
     this.author= author;
     this.description = description;
     this.image = image;
+
+    this.isbn = isbn;
+
+    console.log("value in cons   ***   ", isbn);
 }
-app.listen(PORT, ()=>{
-    console.log('Server is running on port ', PORT);
+
+client.connect().then(data => {
+    app.listen(PORT, () => {
+        console.log('the app is listening to ' + PORT);
+    });
+}).catch(error => {
+    console.log('error in connect to database ' + error);
 });
